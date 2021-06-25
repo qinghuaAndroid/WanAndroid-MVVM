28dacb7d30b84e9b07762f43fbd878147ff81c2b
Alexis.Shelton <Alexis.Shelton@gmail.com>
2021-06-25 16:54:22 +0800

    mvvm样板代码，as自动生成

---
+++ mvvm-examples/src/main/java/com/wan/mvvm_examples/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.wan.mvvm_examples.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
