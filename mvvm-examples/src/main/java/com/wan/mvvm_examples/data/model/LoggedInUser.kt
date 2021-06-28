a7734bd0f1ffbbd7a6edc6050ed2165c8e44b4a4
Alexis.Shelton <Alexis.Shelton@gmail.com>
2021-06-28 11:45:13 +0800

    删除mvvm-examples示例模块

--- mvvm-examples/src/main/java/com/wan/mvvm_examples/data/model/LoggedInUser.kt
+++
@@ -1,9 +0,0 @@
-package com.wan.mvvm_examples.data.model
-
-/**
- * Data class that captures user information for logged in users retrieved from LoginRepository
- */
-data class LoggedInUser(
-    val userId: String,
-    val displayName: String
-)
--- mvvm-examples/src/main/java/com/wan/mvvm_examples/ui/login/LoginResult.kt
+++
@@ -1,9 +0,0 @@
-package com.wan.mvvm_examples.ui.login
-
-/**
- * Authentication result : success (user details) or error message.
- */
-data class LoginResult(
-    val success: LoggedInUserView? = null,
-    val error: Int? = null
-)
