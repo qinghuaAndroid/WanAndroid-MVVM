91e64dc859305ad0700238edc2cf65b850e99f4b
fanqinghua <1204806539@qq.com>
2022-12-23 16:33:58 +0800

    升级依赖

--- app/src/main/java/com/wan/android/app/GlobalViewModel.kt
+++ app/src/main/java/com/wan/android/app/GlobalViewModel.kt
@@ -3,11 +3,6 @@ package com.wan.android.app
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 
-/**
- *
- * Created by qinghua on 2022/11/17.
- * Copyright (c) 2022 Heyooh Technology Co.,Ltd. All rights reserved.
- */
 class GlobalViewModel(application: Application):AndroidViewModel(application) {
 
 }
