00e30dcac6593e57d677092eb10ab47b270248a1
fanqinghua <1204806539@qq.com>
2023-01-28 14:09:57 +0800

    存储、日志工具

--- app/src/main/java/com/wan/android/provider/InitializationProvider.kt
+++ app/src/main/java/com/wan/android/provider/InitializationProvider.kt
@@ -2,6 +2,7 @@ package com.wan.android.provider
 
 import android.app.Application
 import android.content.Context
+import android.util.Log
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.startup.Initializer
 import com.alibaba.android.arouter.launcher.ARouter
@@ -10,12 +11,15 @@ import com.jeremyliao.liveeventbus.LiveEventBus
 import com.kingja.loadsir.core.LoadSir
 import com.tencent.mmkv.MMKV
 import com.wan.android.BuildConfig
+import com.wan.baselib.log.LogUtil
+import com.wan.baselib.storage.StorageType
+import com.wan.baselib.storage.StorageUtil
+import com.wan.baselib.utils.AutoDensityUtils
+import com.wan.baselib.utils.SettingUtil
 import com.wan.common.callback.EmptyCallback
 import com.wan.common.callback.ErrorCallback
 import com.wan.common.callback.LoadingCallback
 import com.wan.common.callback.TimeoutCallback
-import com.wan.baselib.utils.AutoDensityUtils
-import com.wan.baselib.utils.SettingUtil
 
 
 /**
@@ -33,6 +37,11 @@ class InitializationProvider : Initializer<Unit> {
         LiveEventBus.config()
             .lifecycleObserverAlwaysActive(false)
             .autoClear(true)
+        //初始化项目一些图片、视频、日志等文件存储目录
+        StorageUtil.init(context, context.externalCacheDir?.canonicalPath)
+        //初始日志文件存储目录
+        val directory = StorageUtil.getDirectoryByDirType(StorageType.TYPE_LOG)
+        LogUtil.init(directory, Log.DEBUG)
     }
 
     override fun dependencies(): List<Class<out Initializer<*>>> {
